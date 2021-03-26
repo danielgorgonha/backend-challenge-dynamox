3c934a02a16be9d549652089a56ad35df0dd5dd2
Daniel R Gorgonha <rogergorgonha@gmail.com>
2021-03-26 18:10:46 -0300

    Creating User Password Encryption

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -11,7 +12,15 @@ class CreateUserUseCase {
   ) {}
 
   async execute({ name, email, password }: ICreateUserDTO): Promise<void> {
-    await this.usersRepository.create({ name, email, password });
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({ name, email, password: passwordHash });
   }
 }
 
