e0520baebe08bf2bb7c5ea4304a7caf6d8610a0a
Daniel R Gorgonha <rogergorgonha@gmail.com>
2021-03-29 10:25:48 -0300

    Created initial application documentation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "../swgger.json";
 import { authenticateRotues } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { produtsRoutes } from "./products.routes";
@@ -7,6 +9,7 @@ import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 router.use("/users", usersRoutes);
 router.use(authenticateRotues);
 router.use("/categories", categoriesRoutes);
